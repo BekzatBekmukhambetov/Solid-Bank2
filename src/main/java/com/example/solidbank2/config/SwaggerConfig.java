4e486e7cd44984e34108a01f977bd91ae43f7fb3
Bekzat <bekzatxxxbeka@gmail.com>
2024-01-29 20:00:31 +0100

    Merge remote-tracking branch 'origin/feature/delivery' into feature/delivery

--- src/main/java/com/example/solidbank2/config/SwaggerConfig.java
+++ src/main/java/com/example/solidbank2/config/SwaggerConfig.java
@@ -1,7 +1,12 @@
 package com.example.solidbank2.config;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,4 +28,6 @@ public class SwaggerConfig {
                         new Info().title("Jusan Singularity Solid Bank API")
                 );
      }
+
+
 }
